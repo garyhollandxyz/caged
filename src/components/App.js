f9ad6091559e005866fed61986fc485b766d35c2
Gary Holland <garyhollandxyz@gmail.com>
2019-11-26 21:35:04 +0000

    switch to semantic

--- src/components/App.js
+++ src/components/App.js
@@ -1,41 +1,55 @@
-import React, { useState } from "react";
-import { Box, Button, Container, Paper, Typography } from "@material-ui/core";
-const naturalNotes = ["A", "B", "C", "D", "E", "F", "G"];
+import React, { useState, useEffect } from 'react'
+// import { Box, Button, Container, Paper, Typography } from "@material-ui/core";
+import { Button, Container, Header } from 'semantic-ui-react'
+const naturalNotes = ['A', 'B', 'C', 'D', 'E', 'F', 'G']
 
-const shapes = ["C", "A", "G", "E", "D"];
+const shapes = ['C', 'A', 'G', 'E', 'D']
 
-const getRandomItem = array => array[Math.floor(Math.random() * array.length)];
+const getRandomItem = array => array[Math.floor(Math.random() * array.length)]
 
-const getRandomNote = () => getRandomItem(naturalNotes);
+const getRandomNote = () => getRandomItem(naturalNotes)
 
-const getRandomShape = () => getRandomItem(shapes);
+const getRandomShape = () => getRandomItem(shapes)
+
+const getIndefiniteArticle = noteName =>
+  ['A', 'E', 'F'].some(note => note === noteName) ? 'an' : 'a'
+
+const ImportantBit = ({ text }) => (
+  <span style={{ fontSize: '2rem' }}>{text}</span>
+)
 
 export const App = () => {
-  const [root, setRoot] = useState(getRandomNote());
-  const [shape, setShape] = useState(getRandomShape());
+  const [root, setRoot] = useState(getRandomNote())
+  const [shape, setShape] = useState(getRandomShape())
+  const [indefiniteArticle, setIndefiniteArticle] = useState(
+    getIndefiniteArticle(root)
+  )
 
+  useEffect(() => {
+    setIndefiniteArticle(getIndefiniteArticle(root))
+  }, [root])
   return (
-    <Container>
-      <Paper>
-        <Typography variant="h1" component="h1">
-          CAGED practice app
-        </Typography>
-        <Box variant="">
-          <Typography variant="body1">
-            Play a {root} chord using a {shape} shape
-          </Typography>
-        </Box>
-        <Button
-          variant="contained"
-          color="primary"
-          onClick={() => {
-            setRoot(getRandomNote);
-            setShape(getRandomShape);
-          }}
-        >
-          Another!
-        </Button>
-      </Paper>
+    <Container style={{ paddingTop: '1rem' }}>
+      <Header as="h1" content="CAGED practice" />
+      <p> Play {indefiniteArticle} </p>
+      <p>
+        <ImportantBit text={root} />
+      </p>
+      <p>chord using the</p>
+      <p>
+        <ImportantBit text={shape} />
+      </p>
+      <p>shape</p>
+
+      <Button
+        content="Another!"
+        variant="contained"
+        color="primary"
+        onClick={() => {
+          setRoot(getRandomNote)
+          setShape(getRandomShape)
+        }}
+      />
     </Container>
-  );
-};
+  )
+}
