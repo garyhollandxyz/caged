00ff3c090e5535674f6947f36575271b2afd02ce
Gary Holland <garyhollandxyz@gmail.com>
2019-11-26 22:06:22 +0000

    sharps and flats

--- src/components/App.js
+++ src/components/App.js
@@ -2,17 +2,37 @@ import React, { useState, useEffect } from 'react'
 // import { Box, Button, Container, Paper, Typography } from "@material-ui/core";
 import { Button, Container, Header } from 'semantic-ui-react'
 const naturalNotes = ['A', 'B', 'C', 'D', 'E', 'F', 'G']
-
+const modifiers = ['#', '♭', '']
 const shapes = ['C', 'A', 'G', 'E', 'D']
+
+const standardiseName = name => {
+  switch (name) {
+    case 'E#':
+      return 'F'
+    case 'B#':
+      return 'C'
+    case 'F♭':
+      return 'E'
+    case 'C♭':
+      return 'B'
+    default:
+      return name
+  }
+}
 
 const getRandomItem = array => array[Math.floor(Math.random() * array.length)]
 
-const getRandomNote = () => getRandomItem(naturalNotes)
+const getRandomNote = () => {
+  const naturalNote = getRandomItem(naturalNotes)
+  const modifier = getRandomItem(modifiers)
+  const note = `${naturalNote}${modifier}`
+  return standardiseName(note)
+}
 
 const getRandomShape = () => getRandomItem(shapes)
 
 const getIndefiniteArticle = noteName =>
-  ['A', 'E', 'F'].some(note => note === noteName) ? 'an' : 'a'
+  ['A', 'E', 'F'].some(note => noteName.includes(note)) ? 'an' : 'a'
 
 const ImportantBit = ({ text }) => (
   <span style={{ fontSize: '2rem' }}>{text}</span>
